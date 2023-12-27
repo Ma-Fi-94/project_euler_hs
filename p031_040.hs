89ccf9bddd32b9fbefa3631381c1d7c46a66ed95
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2023-12-27 14:39:20 +0100

    Added 035

--- p031_040.hs
+++ p031_040.hs
@@ -30,9 +30,36 @@ p034 = sum . filter check $ [10..10_000_000]
     fac i   = foldl1 (*) [1..i]
 
 
+-- We exclude the cases below 100 (= 13 primes), so we only check
+-- 100 to 999_999. Thus, we can a priori exclude all numbers
+-- that contain a 2,4,6,8,0 ("invalid numbers").
+p035 i
+  |i < 10    = error "Not implemented."
+  |otherwise = (+13)
+             . length
+             . filter (all isPrime . rots)
+             . filter valid
+             $ enumFromTo 100 (i - 1)
+  where
+    valid = all odd . map digitToInt . show
+
+    isPrime i = all ((>0) . (i `rem`)) [2..isqrt i]
+      where
+        isqrt = truncate . sqrt . fromIntegral
+
+    rots :: Int -> [Int]
+    rots n = go len dblstr
+      where
+        len      = length . show $ n 
+        dblstr   = concat . replicate 2 . show $ n
+        go 0 str = []
+        go c str = (read (take len str)) : go (c - 1) (tail str)
+
+
 main = do
     -- print $ "Problem 031: " ++ show (p031 200)
 
     -- print $ "Problem 034: " ++ show p034
+    print $ "Problem 035: " ++ show (p035 1_000_000)
     
     print $ "---------- Done. ----------"
