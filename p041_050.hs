08a7c8bc06dda89031e6a0d224f02475454b5fe0
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2023-12-28 22:48:38 +0100

    042 done

---
+++ p041_050.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE NumericUnderscores #-}
+
+import Data.Char (ord)
+import Utils (tok)
+
+-- The maximum word score is 192, so we just generate the first 200
+-- triangle numbers and check how many words have a score that's
+-- element of that list. An obious improvement would be to use
+-- the quadratic formula to check whether a score is a triangle number.
+-- However, given the tiny search space this is not required at all.
+p042 = length . filter (`elem` triangles) . map wordScore . tok ['"', ',']
+  where
+    wordScore = sum . map ((subtract 64) . ord)
+    triangles = map (\n -> n * (n + 1) `div` 2) [1..200]
+
+main = do
+    input042 <- readFile "0042_words.txt"
+
+    print $ "Problem 042: " ++  show (p042 input042)
+    
+    print $ "---------- Done. ----------"
