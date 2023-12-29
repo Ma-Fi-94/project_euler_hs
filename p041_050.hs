3e723f92ca2fd65cf4e1bbae7fcb68d4d1b869a9
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2023-12-29 17:41:47 +0100

    p042 done

--- p041_050.hs
+++ p041_050.hs
@@ -13,9 +13,17 @@ p042 = length . filter (`elem` triangles) . map wordScore . tok ['"', ',']
     wordScore = sum . map ((subtract 64) . ord)
     triangles = map (\n -> n * (n + 1) `div` 2) [1..200]
 
+
+-- Direct calculation
+p048 = (read :: String -> Int) . reverse . take 10 . reverse . show
+     . sum . map (\n -> n^n) . enumFromTo 1
+
+
 main = do
     input042 <- readFile "0042_words.txt"
 
     print $ "Problem 042: " ++  show (p042 input042)
+
+    print $ "Problem 048: " ++  show (p048 1000)
     
     print $ "---------- Done. ----------"
