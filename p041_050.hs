f3cdf8e8715b8835a838205eacf6e9dee7e0c73b
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2023-12-29 18:34:41 +0100

    046 done

--- p041_050.hs
+++ p041_050.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE NumericUnderscores #-}
 
+
 import Data.Char (ord)
 import Utils (tok)
+
 
 -- The maximum word score is 192, so we just generate the first 200
 -- triangle numbers and check how many words have a score that's
@@ -14,6 +16,19 @@ p042 = length . filter (`elem` triangles) . map wordScore . tok ['"', ',']
     triangles = map (\n -> n * (n + 1) `div` 2) [1..200]
 
 
+-- For every odd composite, we enumerate all possible squares and
+-- check whether the required difference is prime. Runs basically
+-- instantly, so certainly good enough :).
+p046 = head . dropWhile test . filter composite $ [9,11..]
+  where
+    composite i = any ((==0) . (i `rem`)) [2..isqrt i]
+    prime       = not . composite
+    isqrt       = truncate . sqrt . fromIntegral
+    test i      = not . null $ [1 | s     <- map (^2) [1..isqrt i],
+                                    let p =  i - 2 * s,
+                                    prime p]
+
+ 
 -- Direct calculation
 p048 = (read :: String -> Int) . reverse . take 10 . reverse . show
      . sum . map (\n -> n^n) . enumFromTo 1
@@ -22,8 +37,10 @@ p048 = (read :: String -> Int) . reverse . take 10 . reverse . show
 main = do
     input042 <- readFile "0042_words.txt"
 
-    print $ "Problem 042: " ++  show (p042 input042)
+    print $ "Problem 042: " ++ show (p042 input042)
 
-    print $ "Problem 048: " ++  show (p048 1000)
+    print $ "Problem 046: " ++ show p046
+
+    print $ "Problem 048: " ++ show (p048 1000)
     
     print $ "---------- Done. ----------"
