75c4ce505cb57465a4643dcdefdb64b6245143be
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2023-12-31 15:23:02 +0100

    p043 done

--- p041_050.hs
+++ p041_050.hs
@@ -23,6 +23,19 @@ p042 = length . filter (`elem` triangles) . map wordScore . tok ['"', ',']
     triangles = map (\n -> n * (n + 1) `div` 2) [1..200]
 
 
+-- Straightfoward enumeration and check. Runtime of about 6s, which
+-- is not perfect, but still acceptable in my opinion.
+p043 = sum $ filter test candidates
+  where
+    test x     = and
+               . map (\(i, d) -> (subNb i (i+2) x) `rem` d == 0)
+               $ zip [1..7] [2, 3, 5, 7, 11, 13, 17]
+    subNb a b  = readInt . take (b - a + 1) . drop a . show
+    candidates = filter (>=10^9)
+               . map readInt
+               $ permutations "0123456789"
+
+
 -- For every odd composite, we enumerate all possible squares and
 -- check whether the required difference is prime. Runs basically
 -- instantly, so certainly good enough :).
@@ -44,11 +57,12 @@ p048 = (read :: String -> Int) . reverse . take 10 . reverse . show
 main = do
     input042 <- readFile "0042_words.txt"
 
-    print $ "Problem 041: " ++ show (p041)
-    print $ "Problem 042: " ++ show (p042 input042)
+    --print $ "Problem 041: " ++ show (p041)
+    --print $ "Problem 042: " ++ show (p042 input042)
+    print $ "Problem 043: " ++ show p043
 
-    print $ "Problem 046: " ++ show p046
+    --print $ "Problem 046: " ++ show p046
 
-    print $ "Problem 048: " ++ show (p048 1000)
+    --print $ "Problem 048: " ++ show (p048 1000)
     
     print $ "---------- Done. ----------"
