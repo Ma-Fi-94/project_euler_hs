1cb4e6e04eede0ff16d219e428d117ef67467aea
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2023-12-31 23:53:54 +0100

    p059 done -- last one this year :)

---
+++ p051_060.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE NumericUnderscores #-}
+
+import Data.Bits (xor)
+import Data.Char (chr, ord)
+import Data.List (isInfixOf)
+import Utils (readInt, tok)
+
+
+-- Exhaustive search through the space of keys and checking for
+-- the occurence of some common English words.
+p059 input = sum
+           . map ord
+           . head 
+           $ [plain | k1 <- [97..122],
+                      k2 <- [97..122],
+                      k3 <- [97..122],
+                      let plain = decode [k1, k2, k3],
+                      "the" `isInfixOf` plain,
+                      "and" `isInfixOf` plain,
+                      "then" `isInfixOf` plain]
+  where
+    cipherInt  = map readInt . tok "," $ input
+    decode key = map chr $ zipWith xor cipherInt repeatedKey
+      where
+        repeatedKey = concat . repeat $ key
+
+
+main = do
+    input059 <- readFile "0059_cipher.txt"
+
+    print $ "Problem 059: " ++ show (p059 input059)
+
+    print $ "---------- Done. ----------"
