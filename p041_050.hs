10bb091e41f6c83e13baec483b9e273238f14f0e
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2023-12-31 22:05:42 +0100

    Flawless Fifty!

--- p041_050.hs
+++ p041_050.hs
@@ -2,9 +2,9 @@
 
 import Data.Char (ord)
 import Data.Fixed (mod')
-import Data.List (group, inits, intersect, nub, permutations, sort)
+import Data.Ord (comparing)
+import Data.List (group, inits, intersect, maximumBy, nub, permutations, sort, tails)
 import Utils (tok, isPrime, primeFactors, readInt)
-
 
 -- Exhaustive search, based on first generating all n-digit
 -- pandigital numbers for n = 1..9. Runs nearly instantly.
@@ -94,6 +94,48 @@ p048 = (read :: String -> Int) . reverse . take 10 . reverse . show
      . sum . map (\n -> n^n) . enumFromTo 1
 
 
+-- Direct search with lots of constraints for early stopping.
+-- Runs basically instantly.
+p049 = (\(p1, p2, p3) -> p3 + 10000 * p2 + 100000000 * p1)
+     . head
+     $ [(p1, p2, p3) | p1 <- primes,
+                       p1 /= 1487,
+                       p2 <- primes,
+                       p2 > p1,
+                       isPerm p1 p2,
+                       let p3 = p2 + (p2 - p1),
+                       p3 `elem` primes,
+                       isPerm p1 p3]
+  where
+    primes     = filter isPrime [1000..9999]
+    isPerm i j = (sort (show i)) == (sort (show j))
+
+
+-- We start at every individual prime and construct a sublist from it
+-- to the end of the list of all primes. Then, every sublist is shrunken
+-- down from the right until its sum reaches another prime number for
+-- the first time. Then, we return the length of the sublist and the
+-- prime it sums to.
+-- Two optimisations make this run acceptably fast:
+-- First, while shrinking a sublist, we keep track of the current sum,
+-- so we don't have to recalculate the sum from scratch at every step.
+-- Second, we reverse a sublist before attempting to shrink it. In this
+-- way, we have to remove the head, instead of the last element of the
+-- list, which is significantly faster in Haskell.
+-- These two reduce the runtime to 1:46, which is not perfect, but okay.
+-- (Without optimisations, I had to abort after 15min.)
+p050 n = snd
+       . maximumBy (comparing fst)
+       $ map (\l -> shrink (sum l) (reverse l)) listtails
+  where
+    primes       = filter isPrime [2..n-1]
+    listtails    = reverse . init . tails $ primes
+    shrink suml l@(x:xs)
+        |suml > n              = shrink (suml - x) xs
+        |(sum l) `elem` primes = (length l, sum l)
+        |otherwise             = shrink (suml - x) xs
+
+
 main = do
     input042 <- readFile "0042_words.txt"
 
@@ -103,7 +145,9 @@ main = do
     --print $ "Problem 044: " ++ show p044
     --print $ "Problem 045: " ++ show p045
     --print $ "Problem 046: " ++ show p046
-    print $ show p047
+    --print $ "Problem 047: " ++ show p047
     --print $ "Problem 048: " ++ show (p048 1000)
-    
+    --print $ "Problem 049: " ++ show p049
+    print $ "Problem 050: " ++ show (p050 1_000_000)
+
     print $ "---------- Done. ----------"
