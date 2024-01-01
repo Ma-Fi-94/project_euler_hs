5cdd835957602db4bffa5525ccb7cb96eb533e5e
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2024-01-01 17:15:18 +0100

    Two more!

--- p051_060.hs
+++ p051_060.hs
@@ -14,6 +14,15 @@ p052 = head $ [x | x <- [1..],
 
 
 -- Trivial direct search, runs close to instantly.
+p053 i = length . filter (>i) $ [cnk n k | n <- [1..100],
+                                           k <- [1..n]]
+  where
+    cnk n k = (fac n) / ((fac k) * (fac (n-k))) 
+    fac 0   = 1
+    fac i   = foldl1 (*) [1..i]
+
+
+-- Trivial direct search, runs close to instantly.
 p055 = length $ filter isLychrel [1..9999]
   where
     revadd :: Integer -> Integer
@@ -33,8 +42,8 @@ p055 = length $ filter isLychrel [1..9999]
 
 
 -- Exhaustive search is still very fast.
-p056 = maximum [digitSum (a ^ b) | a <- [1..99],
-                                   b <- [1..99]]
+p056 i = maximum [digitSum (a ^ b) | a <- [1..i],
+                                     b <- [1..i]]
   where
     digitSum = sum . map digitToInt . show
 
@@ -60,9 +69,10 @@ main = do
     input059 <- readFile "0059_cipher.txt"
 
     print $ "Problem 052: " ++ show p052
+    print $ "Problem 053: " ++ show (p053 1_000_000)
 
     print $ "Problem 055: " ++ show p055
-    print $ "Problem 056: " ++ show p056
+    print $ "Problem 056: " ++ show (p056 99)
 
     print $ "Problem 059: " ++ show (p059 input059)
 
