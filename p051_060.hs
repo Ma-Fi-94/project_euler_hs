f3e77627a132f016a810908253aa9778a18dcdc8
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2024-01-01 02:34:58 +0100

    Another one!

--- p051_060.hs
+++ p051_060.hs
@@ -3,7 +3,7 @@
 import Data.Bits (xor)
 import Data.Char (chr, ord)
 import Data.List (isInfixOf, sort)
-import Utils (readInt, tok)
+import Utils (readInt, readInteger, tok)
 
 
 -- Trivial direct search, runs close to instantly.
@@ -11,6 +11,25 @@ p052 = head $ [x | x <- [1..],
                    all (\i -> isPermutation x (i*x)) [2..6]]
   where
     isPermutation i j = (sort (show i)) == (sort (show j))
+
+
+-- Trivial direct search, runs close to instantly.
+p055 = length $ filter isLychrel [1..9999]
+  where
+    revadd :: Integer -> Integer
+    revadd i    = i + (readInteger (reverse (show i)))
+
+    isPali :: Integer -> Bool
+    isPali i    = (show i) == (reverse (show i))
+
+    isLychrel :: Integer -> Bool
+    isLychrel i = go 1 (revadd i)
+      where
+        go :: Int -> Integer -> Bool
+        go 50 _ = True
+        go c i
+            |isPali i  = False
+            |otherwise = go (c + 1) (revadd i) 
 
 
 -- Exhaustive search through the space of keys and checking for
@@ -36,6 +55,8 @@ main = do
     input059 <- readFile "0059_cipher.txt"
 
     print $ "Problem 052: " ++ show p052
+
+    print $ "Problem 055: " ++ show p055
 
     print $ "Problem 059: " ++ show (p059 input059)
 
