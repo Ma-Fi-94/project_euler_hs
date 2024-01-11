b536cbdc73d300a6a128eae0b8e28e9cc7954f7e
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2024-01-11 03:27:46 +0100

    p091 -- bruteforce-y O(n^4), but fast enough

--- p091_100.hs
+++ p091_100.hs
@@ -1,8 +1,31 @@
 {-# LANGUAGE NumericUnderscores #-}
 
 import Data.Function (on)
-import Data.List (maximumBy)
+import Data.List (maximumBy, nubBy)
 import Utils (tok)
+
+
+-- This is the most inefficient code conceivable to mankind,
+-- however I utterly failed at getting the right answer using
+-- any more efficient approachwa at half past 3 in the morning.
+-- Compiled for efficiency, it still runs within 3 seconds,
+-- so I am willing to take it (for now).
+p091 n = length
+       . nubBy triEq
+       $ [((x1, y1), (x2, y2)) | x1 <- [0..n],
+                                 y1 <- [0..n],
+                                 x2 <- [0..n],
+                                 y2 <- [0..n],
+                                 let a2 = x1^2 + y1^2,
+                                 let b2 = x2^2 + y2^2,
+                                 let c2 = (y2 - y1) ^ 2 + (x2 - x1) ^ 2,
+                                 a2 > 0, b2 > 0, c2 > 0,
+                                 a2 + b2 == c2 || a2 + c2 == b2 || b2 + c2 == a2]
+  where
+     triEq ((x1, y1), (x2, y2)) ((x1', y1'), (x2', y2')) =
+               (x1, y1) == (x1', y1') && (x2, y2) == (x2', y2')
+            || (x1, y1) == (x2', y2') && (x2, y2) == (x1', y1')
+
 
 -- It is, what it is.
 p097 = (read :: String -> Int)
@@ -23,6 +46,7 @@ p099 = fst
 
 main = do
     input099 <- readFile "0099_base_exp.txt"
+    print $ "Problem 091: " ++ (show $ p091 50)
 
     print $ "Problem 097: " ++ show p097
     print $ "Problem 099: " ++ (show $ p099 input099)
