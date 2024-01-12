227bd32f9ee3596c038124bb3cdfa458fb9afaae
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2024-01-12 18:02:39 +0100

    p100 solved with a bit of research on quadratic Diophantines ^w^

--- p091_100.hs
+++ p091_100.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE NumericUnderscores #-}
 
+import Data.Fixed (mod')
 import Data.Function (on)
-import Data.List (maximumBy, nub)
+import Data.List (maximumBy)
+import Data.Ratio
 import Utils (tok)
 
 
@@ -37,11 +39,37 @@ p099 = fst
      . lines
 
 
+-- I first tried all values of n, calculated the corresponding value of d,
+-- and checked whether p would be exactly 0.5. This was correct, but extremely
+-- inefficient and wouldn't find a solution within 15 minutes.
+-- Let's look at this from another angle:
+-- We want some (n,d) in N, s.t. b*(b-1) / (n*(n-1)) = 0.5.
+-- This reduces to 2b^2 - n^2 - 2b + n = 0, a quadratic Diophantine equation.
+-- From www.alpertron.com.ar/QUAD.HTM, we get that all solutions must satisfy
+-- (b', n') = (3b + 2n - 2, 4b + 3n - 3), starting with (b=1, n=1).
+-- Note that both b' and n' are increasing in both b and n. Also note
+-- that for (1,1) we get new values (3,4). Thus, with every step, n will always
+-- increase strictly. Thus, we may simply enumerate all solutions until we find
+-- the first one where n >= nmin.
+-- This runs in fractions of a second :).
+p100 nmin = go 1 1
+  where
+     go b n
+         |n >= nmin = b
+         |otherwise = go b' n'
+           where
+               b' = 3 * b + 2 * n - 2
+               n' = 4 * b + 3 * n - 3
+
+
 main = do
     input099 <- readFile "0099_base_exp.txt"
-    print $ "Problem 091: " ++ (show $ p091 50)
+    --print $ "Problem 091: " ++ (show $ p091 50)
 
-    print $ "Problem 097: " ++ show p097
-    print $ "Problem 099: " ++ (show $ p099 input099)
+    --print $ "Problem 097: " ++ show p097
+    --print $ "Problem 099: " ++ (show $ p099 input099)
+
+
+    print $ "Problem 100: " ++ (show $ p100 1_000_000_000_001)
 
     print $ "---------- Done. ----------"
