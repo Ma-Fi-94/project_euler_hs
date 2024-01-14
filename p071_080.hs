e26ea9c511ede29f4be5f5406a7591fbf93cc479
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2024-01-14 03:57:20 +0100

    p074 -- straightforward!

--- p071_080.hs
+++ p071_080.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE NumericUnderscores #-}
 
+import Data.Char (digitToInt)
 import Data.List (nub)
+import Data.Maybe (fromJust)
 import Data.Ratio
 import Data.Set (Set)
 import qualified Data.Set as Set
@@ -17,6 +19,7 @@ p071 x dmax = go (0 % 1) (1 % 1)
             m  = (n' % d')
             n' = (numerator l) + (numerator r)
             d' = (denominator l) + (denominator r)
+
 
 -- We only need to sum over the Euler phi function phi(d) for d = 2...dmax.
 -- Due to Möbius, we have an efficient formula for this, which we will use.
@@ -35,9 +38,24 @@ p072 dmax = totientSum dmax - 1
             primefactors = primeFactors k
 
 
+-- We know that the maximum chain is of length 60, so we take the first 60
+-- iterations and check if all elements are unique.
+-- This runs in 22s when compiled for speed, which is acceptable.
+p074 = length . filter allUniq . map (take 60 . iterate sumFac) $ [1..999_999]
+  where
+    allUniq (x:[]) = True
+    allUniq (x:xs) = if   x `elem` xs
+                     then False
+                     else allUnique xs
+    sumFac         = sum . map (fac . digitToInt) . show
+    fac 0          = 1
+    fac n          = foldl1 (*) $ enumFromTo 1 n
+
+
 main = do
-    --print $ "Problem 071: " ++ show (p071 (3 % 7) 1_000_000)
-    print $ "Problem 072: " ++ show (p072 1_000_000)
+    -- print $ "Problem 071: " ++ show (p071 (3 % 7) 1_000_000)
+    -- print $ "Problem 072: " ++ show (p072 1_000_000)
+    print $ "Problem 074: " ++ show p074
 
 
     print $ "---------- Done. ----------"
