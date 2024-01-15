9b11560b0ad63a2672be73859f3bad2449c47694
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2024-01-15 17:56:58 +0100

    p070 -- straightforward extension :)

--- p061_070.hs
+++ p061_070.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE NumericUnderscores #-}
 
 import Data.Function (on)
-import Data.List (group, maximumBy)
+import Data.List (group, maximumBy, minimumBy, sort)
 import Data.Ratio
 import Utils (primeFactors)
 
@@ -20,8 +20,26 @@ p069 = fst
         . primeFactors
 
 
+-- Like before, but we are now looking for the minimum ratio!
+-- We also add the constraint of phi (n) and n being permutations.
+-- We check this efficiently by sorting their digits and testing
+-- for equality. Runs in 20sec, which is okayish.
+p070 = fst
+     . minimumBy (compare `on` snd)
+     . map (\(n, p) -> (n, n % p))
+     . filter isPerm
+     . map (\n -> (n, phi n))
+     . enumFromTo 2
+  where
+    isPerm (n, p) = sort (show n) == sort (show p)
+    phi           = foldl1 (*)
+                  . map (\ps -> (head ps) ^ ((length ps) - 1) * ((head ps) - 1))
+                  . group 
+                  . primeFactors
+
 main = do
-    print $ "Problem 069: " ++ show (p069 1_000_000)
+    --print $ "Problem 069: " ++ show (p069 1_000_000)
+    print $ "Problem 070: " ++ show (p070 10_000_000)
 
 
     print $ "---------- Done. ----------"
